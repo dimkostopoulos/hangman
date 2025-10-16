3c8f8ec4b539252c09ca29a07ead73d955167081
dk <dimkostopoulos@gmail.com>
2025-10-16 23:19:38 +0300

    Create hangman.py

---
+++ src/hangman.py
@@ -0,0 +1,39 @@
+import random
+
+words = ['python', 'java', 'hangman', 'computer', 'keyboard', 'programming']
+word = random.choice(words).lower()
+word_letters = set(word)
+alphabet = set('abcdefghijklmnopqrstuvwxyz')
+used_letters = set()
+lives = 6
+
+# Game loop
+while len(word_letters) > 0 and lives > 0:
+	print(f'\nYou have {lives} lives left')
+	print('Used letters:', ' '.join(used_letters))
+	
+	# Show current word progress
+	word_list = [letter if letter in used_letters else '_' for letter in word]
+	print('Current word:', ' '.join(word_list))
+	
+	user_letter = input('Guess a letter: ').lower()
+	
+	if user_letter in alphabet - used_letters:
+		used_letters.add(user_letter)
+		if user_letter in word_letters:
+			word_letters.remove(user_letter)
+		else:
+			lives -= 1
+			print('Letter is not in word!')
+	
+	elif user_letter in used_letters:
+		print('You already used that letter!')
+	else:
+		print('Invalid character!')
+
+# Game over
+if lives == 0:
+	print(f'\nYou died! The word was: {word}')
+else:
+	print(f'\nCongratulations! You guessed the word: {word}')
+
